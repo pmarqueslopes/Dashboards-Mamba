6d165302e0e764e94c978f228afb27e80bae5389
pmarqueslopes <pmarqueslopes02@gmail.com>
2024-12-29 20:33:43 -0300

    Revert "Dash"
    This reverts commit d09f996618da206b7eeedf35681df798d735d579.

--- Dashboards/config.js
+++
@@ -1,5 +0,0 @@
-const CONFIG = {
-    JSONBIN_API_KEY: '$2a$10$eCMAXi8qsjtD6UrbDhyJRuoaeLK1DI9elOfzuRYqQd8GmOWpCCk5K',
-    JSONBIN_ACCESS_KEY: '$2a$10$1X0lRK2O37wJkE/bGphcGO7.rEj/UFfWTxbYbHPZ0us1s75/U.a8i',
-    JSONBIN_FILE_PATH: 'https://api.jsonbin.io/v3/b/6771dd2ead19ca34f8e2cd6c'
-};
